274ff47f3acbd5487247a619bee119bef2a789ec
Christopher Hamer <developer@monotok.org>
2018-06-27 00:41:51 +0100

    Second unit test

--- test/unit/src/test_gpiocontrol.cpp
+++ test/unit/src/test_gpiocontrol.cpp
@@ -1,9 +1,25 @@
 #include "gtest/gtest.h"
 #include "../../../include/GPIOControl.hpp"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 INITIALIZE_EASYLOGGINGPP
 
 TEST(GPIOControl, DefaultContructor)
 {
-    EXPECT_FALSE(true);
+    GPIOControl gpio = GPIOControl("8");
+    EXPECT_STREQ("8", gpio.get_gpio_num().c_str());
+}
+
+TEST(GPIOControl, GExport)
+{
+    GPIOControl gpio = GPIOControl("8");
+    gpio.g_export();
+
+    struct stat myStat;
+    const char *path_system = "/sys/class/gpio/gpio8";
+    int result = stat(path_system, &myStat);
+    bool isdir = S_ISDIR(myStat.st_mode);
+    EXPECT_TRUE(isdir);
 }
