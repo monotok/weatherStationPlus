43d012ca98d281f6355e4ff4686ad9c110153f42
Christopher Hamer <developer@monotok.org>
2018-07-01 23:29:39 +0100

    Add coverage flags and unit tests for btncontrol

--- test/unit/src/test_btncontrol.cpp
+++ test/unit/src/test_btncontrol.cpp
@@ -1,14 +1,45 @@
 #include "gtest/gtest.h"
+#define private public
 #include "../../../include/BtnControl.hpp"
 
-
-TEST(BTNControl, DefaultConstructor)
+TEST(BTNControl, button_pressed)
 {
     BtnState bs;
-    EXPECT_EQ(bs.lastButtonState, 0);
-    EXPECT_EQ(bs.lastDebounceTime, 0);
-    EXPECT_EQ(bs.debounceDelay, 0);
-    EXPECT_EQ(bs.interruptTime, 0);
-    EXPECT_EQ(bs.storedPinValue, 0);
-    
+    // Don't call the init as it calls GPIOControl which doesn't
+    // exist on this system. We can pretend.
+    bs.setCurrentBtnState(1);
+    bs.setBtnInterruptTime();
+    EXPECT_TRUE(bs.debounceBtn());
+}
+
+TEST(BTNControl, button_pressed_and_held_only_detect_one_press)
+{
+    BtnState bs;
+    // Don't call the init as it calls GPIOControl which doesn't
+    // exist on this system. We can pretend.
+    int b = 0;
+    bs.setCurrentBtnState(1);
+    bs.setBtnInterruptTime();
+    EXPECT_TRUE(bs.debounceBtn());
+    b++;
+    bs.reInitBtnState();
+
+    while (b < 10)
+    {
+        bs.setCurrentBtnState(1);
+        bs.setBtnInterruptTime();
+        EXPECT_FALSE(bs.debounceBtn());
+        b++;
+        bs.reInitBtnState();
+    }
+}
+
+TEST(BTNControl, button_not_pressed)
+{
+    BtnState bs;
+    // Don't call the init as it calls GPIOControl which doesn't
+    // exist on this system. We can pretend.
+    bs.setCurrentBtnState(0);
+    bs.setBtnInterruptTime();
+    EXPECT_FALSE(bs.debounceBtn());
 }
