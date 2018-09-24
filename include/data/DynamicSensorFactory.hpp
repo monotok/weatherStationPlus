311441039af15b47f7de511f721a13b9109eca84
Christopher Hamer <developer@monotok.org>
2018-09-24 20:57:58 +0100

    Add dynamic sensor factory and test

---
+++ include/data/DynamicSensorFactory.hpp
@@ -0,0 +1,10 @@
+#ifndef DYNAMIC_SEN_FACTORY_H
+#define DYNAMIC_SEN_FACTORY_H
+
+class DynamicSensorFactory
+{
+  public:
+  private:
+};
+
+#endif //ifndef DYNAMIC_SEN_FACTORY_H
---
+++ test/unit/src/test_dynamicsensorfactory.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+#include <iostream>
+#include "../../../include/RetrieveSenData.hpp"
+#include "../../../include/i2cControl.hpp"
+#include "../../../include/data/WeatherSensor.hpp"
+#include "../../../include/data/DynamicSensorFactory.hpp"
