4d2306239431d3f810ad5fe19944c6a0c741facf
Christopher Hamer <developer@monotok.org>
2018-09-25 00:44:23 +0100

    Created first test of dynamic sensor factory

--- test/unit/src/test_dynamicsensorfactory.cpp
+++ test/unit/src/test_dynamicsensorfactory.cpp
@@ -1,6 +1,19 @@
 #include "gtest/gtest.h"
+#define private public
+
 #include <iostream>
-#include "../../../include/RetrieveSenData.hpp"
-#include "../../../include/i2cControl.hpp"
 #include "../../../include/data/WeatherSensor.hpp"
 #include "../../../include/data/DynamicSensorFactory.hpp"
+
+TEST(DynamicSensorFactory, Add_new_weatherSensor_obj_to_vector)
+{
+    WeatherSensor *tempSensor = new WeatherSensor();
+    tempSensor->weatherSensorUnion.tsd.sensorID[0] = 'T';
+    tempSensor->weatherSensorUnion.tsd.temperature = 23;
+    tempSensor->weatherSensorUnion.tsd.perBatt = 60;
+
+    DynamicSensorFactory dsf = DynamicSensorFactory();
+    dsf.CreateNewWeatherSensor_obj(tempSensor);
+
+    EXPECT_EQ("Here", dsf.weatherSensors_vector.at(0)->get_sensorID());
+}
