fd87ea85f59462270cbbae50c6648e3ee5a76725
Chris Hamer <developer@monotok.org>
2019-09-30 08:15:34 +0100

    added todo

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include "../include/easylogging++.hpp"
 INITIALIZE_EASYLOGGINGPP
 
+//TODO: Setup threading to run all the background stuff
+//TODO: Get data from arduino (restrieveSenData)
+//TODO: Add logging to all the functions like LCD controller.
+//TODO: Define what the buttons do to control different bits eg. Change page.
+//TODO: Setup lcd driver
+//TODO: Build the sensors based on the incoming data from the arduino
 
 int main(int argc, char** argv)
 {
