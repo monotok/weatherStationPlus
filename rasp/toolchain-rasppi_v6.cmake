e3e936da86ef7941d9e9fe5446e9260b574d9810
Christopher Hamer <developer@monotok.org>
2019-11-04 00:58:03 +0000

    Revert "using the standard apt compilers"
    This reverts commit 9e0e763b
    
    and use new compiler

---
+++ rasp/toolchain-rasppi_v6.cmake
@@ -0,0 +1,23 @@
+SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_SYSTEM_VERSION 1)
+
+# Specify the cross compiler
+SET(CMAKE_C_COMPILER $ENV{HOME}/SoftwareDevelopment/Libaries/arm-crosscompiler/cross-pi-gcc-6.3.0-0/bin/arm-linux-gnueabihf-gcc-6.3.0)
+SET(CMAKE_CXX_COMPILER $ENV{HOME}/SoftwareDevelopment/Libaries/arm-crosscompiler/cross-pi-gcc-6.3.0-0/bin/arm-linux-gnueabihf-g++)
+
+# Where is the target environment
+SET(CMAKE_FIND_ROOT_PATH $ENV{HOME}/SoftwareDevelopment/Libaries/arm-crosscompiler/cross-pi-gcc-6.3.0-0/arm-linux-gnueabihf)
+#SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --sysroot=${CMAKE_FIND_ROOT_PATH}")
+#SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} --sysroot=${CMAKE_FIND_ROOT_PATH}")
+#SET(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} --sysroot=${CMAKE_FIND_ROOT_PATH}")
+
+SET(CMAKE_CXX_FLAGS "-DELPP_NO_DEFAULT_LOG_FILE -g -O0 -Wall -fprofile-arcs -ftest-coverage" CACHE STRING "" FORCE)
+SET(CMAKE_C_FLAGS "-DELPP_NO_DEFAULT_LOG_FILE -g -O0 -Wall -W -fprofile-arcs -ftest-coverage" CACHE STRING "" FORCE)
+SET(CMAKE_EXE_LINKER_FLAGS "-fprofile-arcs -ftest-coverage" CACHE STRING "" FORCE)
+
+# Search for programs only in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# Search for libraries and headers only in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
