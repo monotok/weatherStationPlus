f2681760cc4498bed261d73bea6b2ab46bc132ea
Chris Hamer <developer@monotok.org>
2021-11-13 01:00:55 +0000

    update tests to reflect using yang

--- test/unit/src/test_configParser.cpp
+++ test/unit/src/test_configParser.cpp
@@ -4,23 +4,27 @@
 TEST(ConfigParser, read_version_number)
 {
     Settings weatherStationSettings {};
-    ConfigParser conf(weatherStationSettings, "../../settings.conf");
+    ConfigParser conf(weatherStationSettings, "test/settings.conf", "test/settings.xml");
     conf.ParseConfiguration();
-    EXPECT_EQ(weatherStationSettings.version, 2.0);
+    EXPECT_EQ(weatherStationSettings.version, 3.0);
 }
 
 TEST(ConfigParser, get_database_details)
 {
     Settings weatherStationSettings {};
-    ConfigParser conf(weatherStationSettings, "../../settings.conf");
+    ConfigParser conf(weatherStationSettings, "test/settings.conf", "test/settings.xml");
     conf.ParseConfiguration();
     EXPECT_EQ(weatherStationSettings.db.host, "172.16.20.5");
+    EXPECT_EQ(weatherStationSettings.db.port, 5432);
+    EXPECT_EQ(weatherStationSettings.db.database, "test_weatherdb");
+    EXPECT_EQ(weatherStationSettings.db.user, "test_weather");
+    EXPECT_EQ(weatherStationSettings.db.password, "testtest");
 }
 
 TEST(ConfigParser, get_gpio_details)
 {
     Settings weatherStationSettings {};
-    ConfigParser conf(weatherStationSettings, "../../settings.conf");
+    ConfigParser conf(weatherStationSettings, "test/settings.conf", "test/settings.xml");
     conf.ParseConfiguration();
     EXPECT_EQ(weatherStationSettings.gpio.gpio3, "22");
 }
@@ -28,7 +32,7 @@ TEST(ConfigParser, get_gpio_details)
 TEST(ConfigParser, get_logging_details)
 {
     Settings weatherStationSettings {};
-    ConfigParser conf(weatherStationSettings, "../../settings.conf");
+    ConfigParser conf(weatherStationSettings, "test/settings.conf", "test/settings.xml");
     conf.ParseConfiguration();
     EXPECT_EQ(weatherStationSettings.logg.configFile, "conf/logging.conf");
 }
@@ -36,7 +40,7 @@ TEST(ConfigParser, get_logging_details)
 TEST(ConfigParser, get_sensor2_details)
 {
     Settings weatherStationSettings {};
-    ConfigParser conf(weatherStationSettings, "../../settings.conf");
+    ConfigParser conf(weatherStationSettings, "test/settings.conf", "test/settings.xml");
     EXPECT_EQ(conf.getSensorsName("1"), "Backbed");
     EXPECT_EQ(conf.getSensorsName("2"), "Shed");
 }
@@ -44,14 +48,29 @@ TEST(ConfigParser, get_sensor2_details)
 TEST(ConfigParser, get_invalid_sensor_name)
 {
     Settings weatherStationSettings {};
-    ConfigParser conf(weatherStationSettings, "../../settings.conf");
+    ConfigParser conf(weatherStationSettings, "test/settings.conf", "test/settings.xml");
     EXPECT_TRUE(conf.getSensorsName("5").empty());
+}
+
+TEST(ConfigParser, get_sensor_reading_name)
+{
+    Settings weatherStationSettings {};
+    ConfigParser conf(weatherStationSettings, "test/settings.conf", "test/settings.xml");
+    EXPECT_EQ(conf.getSensorReadingName("1", "1.0"), "Temp");
+    EXPECT_EQ(conf.getSensorReadingName("1", "1.1"), "Hum");
+}
+
+TEST(ConfigParser, get_invalid_sensor_reading_name)
+{
+    Settings weatherStationSettings {};
+    ConfigParser conf(weatherStationSettings, "test/settings.conf", "test/settings.xml");
+    EXPECT_EQ(conf.getSensorReadingName("1", "1.7"), "");
 }
 
 TEST(ConfigParser, get_sensor_reading_position)
 {
     Settings weatherStationSettings {};
-    ConfigParser conf(weatherStationSettings, "../../settings.conf");
+    ConfigParser conf(weatherStationSettings, "test/settings.conf", "test/settings.xml");
     conf.ParseConfiguration();
     Position topleft = {2, 5};
     Position middleright= {3, 15};
@@ -62,7 +81,7 @@ TEST(ConfigParser, get_sensor_reading_position)
 TEST(ConfigParser, get_sensor_reading_position_non_existent)
 {
     Settings weatherStationSettings {};
-    ConfigParser conf(weatherStationSettings, "../../settings.conf");
+    ConfigParser conf(weatherStationSettings, "test/settings.conf", "test/settings.xml");
     conf.ParseConfiguration();
     Position topleft_default = {2, 5};
     Position middleright = {3, 15};
@@ -73,7 +92,7 @@ TEST(ConfigParser, get_sensor_reading_position_non_existent)
 TEST(ConfigParser, get_position_information)
 {
     Settings weatherStationSettings {};
-    ConfigParser conf(weatherStationSettings, "../../settings.conf");
+    ConfigParser conf(weatherStationSettings, "test/settings.conf", "test/settings.xml");
     conf.ParseConfiguration();
     EXPECT_EQ(weatherStationSettings.topleft_Name.row_start, 2);
     EXPECT_EQ(weatherStationSettings.topleft_Val.row_start, 2);
--- test/unit/src/test_yangparser.cpp
+++ test/unit/src/test_yangparser.cpp
@@ -3,16 +3,40 @@
 
 TEST(YangParser, initialise_yang_parser_settings)
 {
-    ASSERT_NO_THROW(YangParser settings_yp("settings", "test/settings.xml"));
+    ASSERT_NO_THROW
+    (
+            YangParser settings_yp("settings");
+            settings_yp.parseData("test/settings.xml");
+    );
+}
+
+TEST(YangParser, initialise_yang_parser_settings_without_data)
+{
+    ASSERT_NO_THROW
+    (
+            YangParser settings_yp("settings");
+    );
 }
 
 TEST(YangParser, find_sensor_name_from_id)
 {
-    YangParser settings_yp("settings", "test/settings.xml");
-    string sensorName1 = settings_yp.getSensorsName("1");
+    YangParser settings_yp("settings");
+    settings_yp.parseData("test/settings.xml");
+    string xpath_str1 = "/settings:settings/sensors/wsensor[id='1']/name";
+    string sensorName1 = settings_yp.getSpecificValue(xpath_str1);
     ASSERT_STREQ(sensorName1.c_str(), "Backbed");
 
-    string sensorName2 = settings_yp.getSensorsName("2");
+    string xpath_str2 = "/settings:settings/sensors/wsensor[id='2']/name";
+    string sensorName2 = settings_yp.getSpecificValue(xpath_str2);
     ASSERT_STREQ(sensorName2.c_str(), "Shed");
+}
+
+TEST(YangParser, find_sensor_reading_name_via_reading_id)
+{
+    YangParser settings_yp("settings");
+    settings_yp.parseData("test/settings.xml");
+    string xpath_str = "/settings:settings/sensors/wsensor[id='1']/readings[id='1.0']/name";
+    string sensorReadingName1 = settings_yp.getSpecificValue(xpath_str);
+    ASSERT_STREQ(sensorReadingName1.c_str(), "Temp");
 }
 
